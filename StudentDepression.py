c88bb70923a4ede00232eed73d67ce2ae9600377
Kaylaaa55 <ameliaputrikayla47@gmail.com>
2025-05-01 20:10:21 +0700

    Update StudentDepression.py

--- StudentDepression.py
+++ StudentDepression.py
@@ -51,8 +51,15 @@ By detecting early signs of depression, this tool aims to support mental health 
 """)
 
 # Load model
-with open("StudentDepression4.pkl", "rb") as file:
+#with open("StudentDepression4.pkl", "rb") as file:
     model = pickle.load(file)
+
+@st.cache_resource
+def load_model():
+    with open("StudentDepression4.pkl", "rb") as file:
+        return pickle.load(file)
+
+model = load_model()
 
 # Form input data
 st.header("Input Student's Data:")
